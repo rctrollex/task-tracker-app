5939be2e0cb46433e8a930fd6a41619de53d777d
rctrollex <rctrollex@gmail.com>
2025-05-03 21:50:47 +0200

    Added Tasklist Ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import Header from "./components/Header.jsx";
 import AddTask from "./components/AddTask.jsx";
+import TaskList from "./components/TaskList.jsx";
 
 const App = () => {
     const [showModal, setShowModal] = useState(false)
@@ -8,6 +9,7 @@ const App = () => {
         <div className = "bg-gray-100 min-h-screen p-4 sm:p-6">
             <div className = "max-w-3xl mx-auto bg-white shadow-md rounded-md p-6">
                 <Header onAddClick={() => setShowModal(true)}/>
+                <TaskList/>
 
             </div>
             {showModal && <AddTask onClose = {()=> setShowModal(false)}/>}
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const TaskList = () => {
+    return (
+        <ul className ="space-y-4">
+            <li className = "bg-gray-100 px-4 py-2 border-1 border-gray-300 rounded-2xl flex items-start justify-between">
+                <div>
+                    <p className = "text-gray-800 font-medium">Finish Appwrite Setup</p>
+                    <span className = "text-xs text-gray-600">Catergory: Work</span>
+                </div>
+                <div className="flex items-center gap-2">
+                    <button className="text-green-500 hover:text-green-700" title="Mark Complete">✔️</button>
+                    <button className="text-blue-500 hover:text-blue-700" title="Edit Task">✏️</button>
+                    <button className="text-red-500 hover:text-red-700" title="Delete Task">🗑️</button>
+                </div>
+            </li>
+            <li className="bg-gray-100 px-4 py-2 border-1 border-gray-300 rounded-2xl flex items-start justify-between">
+                <div>
+                    <p className="text-gray-800 font-medium line-through">Buy groceries</p>
+                    <span className="text-xs text-gray-500">Category: Personal</span>
+                </div>
+                <div className="flex items-center gap-2">
+                    <button className="text-green-500 hover:text-green-700" title="Mark Complete">✔️</button>
+                    <button className="text-blue-500 hover:text-blue-700" title="Edit Task">✏️</button>
+                    <button className="text-red-500 hover:text-red-700" title="Delete Task">🗑️</button>
+                </div>
+            </li>
+        </ul>
+
+    )
+}
+export default TaskList
