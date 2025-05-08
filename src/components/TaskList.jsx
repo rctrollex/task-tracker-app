3246c56093403bd71ebc5c1264d3857b4d05d63d
rctrollex <rctrollex@gmail.com>
2025-05-09 01:17:28 +0200

    Added icons

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,6 +2,9 @@ import React, {useEffect, useState} from 'react'
 import {databaseId,databases,collectionId} from "../appwrite/appwriteConfig.js";
 import {ThreeDot} from "react-loading-indicators";
 import AddTask from "./AddTask.jsx";
+import EditIcon from "../assets/edit.svg"
+import DeleteIcon from "../assets/bin.svg"
+import DoneIcon from "../assets/done.svg"
 
 const TaskList = ({refetchTrigger, filterCategory}) => {
     const [tasks, setTasks] = useState([]);
@@ -104,9 +107,15 @@ const TaskList = ({refetchTrigger, filterCategory}) => {
                             <span className="text-xs text-gray-600">Category: {task.category}</span>
                         </div>
                         <div className="flex items-center gap-2">
-                            <button className="text-green-500 hover:text-green-700" title="Mark Complete" onClick={()=> handleComplete(task.$id, task.isCompleted)}>✔️</button>
-                            <button className="text-blue-500 hover:text-blue-700" title="Edit Task" onClick={() => handleEdit(task)}>✏️</button>
-                            <button className="text-red-500 hover:text-red-700" title="Delete Task" onClick={() => handleDelete(task.$id)}>🗑️</button>
+                            <button className="text-green-500 hover:text-green-700" title="Mark Complete" onClick={()=> handleComplete(task.$id, task.isCompleted)}>
+                                <img src={DoneIcon} alt = "Delete Icon" className="w-[30px] h-[30px] cursor-pointer"/>
+                            </button>
+                            <button className="text-blue-500 hover:text-blue-700" title="Edit Task" onClick={() => handleEdit(task)}>
+                                <img src={EditIcon} alt = "Edit Icon" className="w-[40px] h-[40px] cursor-pointer"/>
+                            </button>
+                            <button className="text-red-500 hover:text-red-700" title="Delete Task" onClick={() => handleDelete(task.$id)}>
+                                <img src={DeleteIcon} alt = "Delete Icon" className="w-[30px] h-[30px] cursor-pointer"/>
+                            </button>
                         </div>
 
                     </li>
